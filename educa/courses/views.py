463743ce7c9832a07c3da0abffb4f394a5c3957d
Elias M. Mwita <eliasmwita86@gmail.com>
2025-09-14 02:12:09 +0300

    Creating class-based views

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,8 +1,11 @@
 from django.views.generic.list import ListView
 from .models import Course
+
+
 class ManageCourseListView(ListView):
- model = Course
- template_name = 'courses/manage/course/list.html'
- def get_queryset(self):
- qs = super().get_queryset()
- return qs.filter(owner=self.request.user)
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
