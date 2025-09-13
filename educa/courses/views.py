c8e60fbbe88a2c4eced1b5ce04bab0aaf24a99ec
Elias M. Mwita <eliasmwita86@gmail.com>
2025-09-14 02:33:05 +0300

    Working with groups and permissions

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -3,6 +3,8 @@ from django.views.generic.edit import CreateView, \
     UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Course
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+    PermissionRequiredMixin
 
 
 class OwnerMixin:
@@ -17,7 +19,9 @@ class OwnerEditMixin:
         return super().form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin,
+                       LoginRequiredMixin,
+                       PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
     success_url = reverse_lazy('manage_course_list')
@@ -29,15 +33,17 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
 
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = 'courses.add_course'
 
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+    permission_required = 'courses.change_course'
 
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
