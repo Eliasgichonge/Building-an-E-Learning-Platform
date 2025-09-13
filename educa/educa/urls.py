6f1bbeaa6f890cb28d54da2bd0cd3220d1f1e139
Elias M. Mwita <eliasmwita86@gmail.com>
2025-09-14 02:35:55 +0300

    Working with groups and permissions

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +8,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
 
 # Serve media files during development
