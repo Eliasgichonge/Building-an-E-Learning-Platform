56998622a4f252a37ab8e138cfbba13a2f0794f9
Elias M. Mwita <eliasmwita86@gmail.com>
2025-09-14 03:13:51 +0300

    Working with groups and permissions

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -5,10 +5,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    # Authentication
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout'),
+
+    # Admin
     path('admin/', admin.site.urls),
-    path('course/', include('courses.urls')),
+
+    # Courses app
+    path('course/', include('courses.urls')),  # Includes all course-related URLs
 ]
 
 # Serve media files during development
